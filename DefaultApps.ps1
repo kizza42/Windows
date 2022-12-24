582082b0ac06d2856f7e188b55f7bf2f28161bb5
Kieran Vella <kizonline@the-vellas.com>
2022-12-24 21:32:57 +1100

    Create DefaultApps.ps1

---
+++ DefaultApps.ps1
@@ -0,0 +1,12 @@
+#Default Dependencies
+winget install --force --id Google.Chrome --source winget;
+winget install --force --id Google.Drive --source winget;
+winget install --force --id 7zip.7zip --source winget;
+winget install --force --id HermannSchinagl.LinkShellExtension --source winget;
+winget install --force --id AntibodySoftware.WizTree --source winget;
+winget install --force --id Microsoft.PowerToys --source winget;
+winget install --force --id ShareX.ShareX --source winget;
+winget install --force --id AdrienAllard.FileConverter --source winget;
+winget install --force --id SomePythonThings.WingetUIStore --source winget;
+winget install --force --id Microsoft.VisualStudioCode --source winget;
+winget install --force --id SyncTrayzor.SyncTrayzor --source winget;
